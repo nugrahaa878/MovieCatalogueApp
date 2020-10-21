cee896cc6a124d36ad8ba8fa6016292104344c27
Ari Nugraha <nugrahaa878@gmail.com>
2020-10-21 10:45:57 +0700

    finish xml for detail activity

--- app/src/main/java/com/nugrahaa/moviecatalogue/model/MovieEntity.kt
+++ app/src/main/java/com/nugrahaa/moviecatalogue/model/MovieEntity.kt
@@ -2,6 +2,7 @@ package com.nugrahaa.moviecatalogue.model
 
 data class MovieEntity(
     var title: String,
+    var userscore: String,
     var date: String,
     var genre: String,
     var description: String,
--- app/src/main/java/com/nugrahaa/moviecatalogue/utils/DataDummy.kt
+++ app/src/main/java/com/nugrahaa/moviecatalogue/utils/DataDummy.kt
@@ -11,66 +11,77 @@ object DataDummy {
         val movies = ArrayList<MovieEntity>()
 
         movies.add(MovieEntity("A Star Is Born",
+            "75",
             "Oktober 3, 2018",
             "Drama, Romance, Music",
             "Seasoned musician Jackson Maine discovers — and falls in love with — struggling artist Ally. She has just about given up on her dream to make it big as a singer — until Jack coaxes her into the spotlight. But even as Ally's career takes off, the personal side of their relationship is breaking down, as Jack fights an ongoing battle with his own internal demons.",
             "2h 16m", R.drawable.poster_a_start_is_born))
 
         movies.add(MovieEntity("Alita: Battle Angel",
+            "71",
             "Januari 31, 2019",
             "Action, Science Fiction, Adventure",
             "When Alita awakens with no memory of who she is in a future world she does not recognize, she is taken in by Ido, a compassionate doctor who realizes that somewhere in this abandoned cyborg shell is the heart and soul of a young woman with an extraordinary past.",
             "2h 2m", R.drawable.poster_alita))
 
         movies.add(MovieEntity("Aquaman",
+            "69",
             "Desember 7, 2018",
             "Action, Adventure, Fantasy",
             "Once home to the most advanced civilization on Earth, Atlantis is now an underwater kingdom ruled by the power-hungry King Orm. With a vast army at his disposal, Orm plans to conquer the remaining oceanic people and then the surface world. Standing in his way is Arthur Curry, Orm's half-human, half-Atlantean brother and true heir to the throne.",
             "2h 24m", R.drawable.poster_aquaman))
 
         movies.add(MovieEntity("Bohemian Rhapsody",
+            "80",
             "Oktober 24, 2018",
             "Drama, Music",
             "Singer Freddie Mercury, guitarist Brian May, drummer Roger Taylor and bass guitarist John Deacon take the music world by storm when they form the rock 'n' roll band Queen in 1970. Hit songs become instant classics. When Mercury's increasingly wild lifestyle starts to spiral out of control, Queen soon faces its greatest challenge yet – finding a way to keep the band together amid the success and excess.",
             "2h 15m", R.drawable.poster_bohemian))
 
         movies.add(MovieEntity("Cold Pursuit",
+            "56",
             "Februari 7, 2019",
             "Action, Crime, Thriller",
             "The quiet family life of Nels Coxman, a snowplow driver, is upended after his son's murder. Nels begins a vengeful hunt for Viking, the drug lord he holds responsible for the killing, eliminating Viking's associates one by one. As Nels draws closer to Viking, his actions bring even more unexpected and violent consequences, as he proves that revenge is all in the execution.",
             "1h 59m", R.drawable.poster_cold_persuit))
 
         movies.add(MovieEntity("Creed",
+            "69",
             "November 25, 2015",
             "Drama",
             "The former World Heavyweight Champion Rocky Balboa serves as a trainer and mentor to Adonis Johnson, the son of his late friend and former rival Apollo Creed.",
             "2h 13m", R.drawable.poster_creed))
 
         movies.add(MovieEntity("Fantastic Beasts: The Crimes of Grindelwald",
+            "69",
             "November 14, 2018",
             "Adventure, Fantasy, Drama",
             "Gellert Grindelwald has escaped imprisonment and has begun gathering followers to his cause—elevating wizards above all non-magical beings. The only one capable of putting a stop to him is the wizard he once called his closest friend, Albus Dumbledore. However, Dumbledore will need to seek help from the wizard who had thwarted Grindelwald once before, his former student Newt Scamander, who agrees to help, unaware of the dangers that lie ahead. Lines are drawn as love and loyalty are tested, even among the truest friends and family, in an increasingly divided wizarding world.",
             "2h 14m", R.drawable.poster_crimes))
 
         movies.add(MovieEntity("Glass",
+            "72",
             "Januari 16, 2019",
             "Thriller, Drama, Science Fiction",
             "In a series of escalating encounters, former security guard David Dunn uses his supernatural abilities to track Kevin Wendell Crumb, a disturbed man who has twenty-four personalities. Meanwhile, the shadowy presence of Elijah Price emerges as an orchestrator who holds secrets critical to both men.",
             "2h 9m", R.drawable.poster_glass))
 
         movies.add(MovieEntity("How to Train Your Dragon: The Hidden World",
+            "81",
             "Januari 3, 2019",
             "Animation, Family, Adventure",
             "As Hiccup fulfills his dream of creating a peaceful dragon utopia, Toothless’ discovery of an untamed, elusive mate draws the Night Fury away. When danger mounts at home and Hiccup’s reign as village chief is tested, both dragon and rider must make impossible decisions to save their kind.",
             "1h 44m", R.drawable.poster_how_to_train))
 
         movies.add(MovieEntity("Avengers: Infinity War",
+            "82",
             "April 25, 2018",
             "Adventure, Action, Science Fiction",
             "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
             "2h 29m", R.drawable.poster_infinity_war))
 
         movies.add(MovieEntity("Spider-Man: Into the Spider-Verse",
+            "83",
             "Desember 6, 2018",
             "Action, Adventure, Animation, Science Fiction, Comedy",
             "Miles Morales is juggling his life between being a high school student and being a spider-man. When Wilson \"Kingpin\" Fisk uses a super collider, others from across the Spider-Verse are transported to this dimension.",
